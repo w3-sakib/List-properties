79ea3e341cd89504656e432b425c29d33e8354e3
w3-sakib <84985523+w3-sakib@users.noreply.github.com>
2023-05-04 18:18:56 +0600

    Add files via upload

---
+++ List properties.dart
@@ -0,0 +1,25 @@
+void main(){
+  var city=['Dhaka','Rangpur','Sylhet,Khulna','Mymensingh','Barishal','Chottogram','Rajshahi'];
+  var result=city.length;
+  print(result);
+
+  var vilege=['Dhaka','Rangpur','Sylhet,Khulna','Mymensingh','Barishal','Chottogram','Rajshahi'];
+  var sdf=vilege.reversed;
+  print(sdf);
+
+
+  var xyz=['Dhaka','Rangpur','Sylhet,Khulna','Mymensingh','Barishal','Chottogram','Rajshahi'];
+  var a=xyz.isNotEmpty;
+  print(a);
+
+
+  var abc=['Dhaka','Rangpur','Sylhet,Khulna','Mymensingh','Barishal','Chottogram','Rajshahi'];
+  var d=abc.isEmpty;
+  print(d);
+
+  var def=['Dhaka'];
+  var z=def.single;
+  print(z);
+
+
+}
